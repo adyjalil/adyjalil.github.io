e0925a2c1df5a80b2056c1564ebf227548cdd60f
Ady Jalil <adysyukri@gmail.com>
2018-08-04 12:03:01 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>{{welcome}}</h1>
+        <h2>{{myName}}</h2>
+    </header>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            welcome: 'Welcome to my portfolio page',
+            myName: 'I am Ady Jalil!'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        text-align: center;
+        background-color: blueviolet;
+        align-items: center;
+        width: 70%;
+        margin: auto;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    h1 {
+        margin-top: 0;
+        
+    }
+    h2 {
+        margin-bottom: 0;
+    }
+    
+</style>
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="nav in navlink">{{nav}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            navlink: ['About Me', 'Education', 'Certificates', 'Projects', 'Contacts']
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        justify-content: center;
+        margin: 0;
+        padding: 0%;
+        align-items: flex-start;
+    }
+    
+    div > ul {
+        list-style-type: none;
+        display: flex;
+        background-color: blanchedalmond;
+        justify-content: space-between;
+        margin: 0;
+    }
+
+    li {
+        flex: 1;
+        background-color: aqua;
+        
+    }
+</style>
+
